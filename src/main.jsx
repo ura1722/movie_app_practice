29bc0b997fca8609290d318ac2776ec5668b614e
ura1722 <birkovych.yurii@student.uzhnu.edu.ua>
2024-06-17 21:19:26 +0300

    commit_10

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App.jsx'
@@ -8,8 +8,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <HashRouter>
+    
       <App />
-    </HashRouter>
+    
   </Provider>,
 )
